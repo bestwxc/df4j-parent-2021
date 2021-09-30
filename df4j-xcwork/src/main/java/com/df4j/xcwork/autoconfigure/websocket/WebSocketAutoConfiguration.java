1b41807269ad09e30d801e486962bb0754cd5410
王学成 <wangxuecheng@dgzq.com.cn>
2021-09-30 09:53:20 +0800

    调整依赖， 使基本项目跑起来

--- df4j-xcwork/src/main/java/com/df4j/xcwork/autoconfigure/websocket/WebSocketAutoConfiguration.java
+++ df4j-xcwork/src/main/java/com/df4j/xcwork/autoconfigure/websocket/WebSocketAutoConfiguration.java
@@ -3,8 +3,11 @@ package com.df4j.xcwork.autoconfigure.websocket;
 import com.df4j.xcwork.base.constant.Constants;
 import com.df4j.xcwork.websocket.XcworkTextWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +16,14 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import javax.websocket.server.ServerContainer;
 import java.util.List;
 
 @Configuration
 @EnableWebSocket
 @EnableConfigurationProperties(WebSocketProperties.class)
+@AutoConfigureAfter(WebSocketServletAutoConfiguration.class)
+@ConditionalOnClass({ServerContainer.class, WebSocketConfigurer.class})
 @ConditionalOnProperty(prefix = Constants.FRAME_CODE + ".websocket", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class WebSocketAutoConfiguration implements WebSocketConfigurer {
 
